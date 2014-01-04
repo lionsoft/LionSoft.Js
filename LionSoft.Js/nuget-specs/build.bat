176b9ee9fee68d92305199ab86e200263c796473
lionsoft <eleybov@gmail.com>
2014-01-04 12:48:44 +0200

    Add TypeScript Definitely NuGet package

--- LionSoft.Js/nuget-specs/build.bat
+++ LionSoft.Js/nuget-specs/build.bat
@@ -1,3 +1,5 @@
 set ProjectDir=%~1
 "%ProjectDir%..\.nuget\nuget" pack "%ProjectDir%nuget-specs\Lionsoft.Js.nuspec" /OutputDirectory "%ProjectDir%nuget-packages"
+"%ProjectDir%..\.nuget\nuget" pack "%ProjectDir%nuget-specs\LionSoft.Js.TypeScript.DefinitelyTyped.nuspec" /OutputDirectory "%ProjectDir%nuget-packages"
+
 
